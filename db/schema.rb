fbfc29474cfcbb82369f5bc290ce181542521f6d
Iwada Bassey <basmo13@yahoo.co.uk>
2012-02-11 23:49:35 -0800

    Login Modifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111130171402) do
+ActiveRecord::Schema.define(:version => 20120212030413) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(:version => 20111130171402) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.integer  "failed_logins_count",             :default => 0
+    t.datetime "lock_expires_at"
   end
 
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
